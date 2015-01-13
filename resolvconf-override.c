dc9dff7ec5c151c7578499bb18ea7d5ca265cee5
eddiehung <e.hung@imperial.ac.uk>
2015-01-13 16:50:44 +0000

    Catch gethostbyname_r() failures and retry with suffix

--- resolvconf-override.c
+++ resolvconf-override.c
@@ -14,78 +14,22 @@
 #include <string.h>
 #include <stdlib.h>
 
-/* For debugging */
-#if 0
-static void
-print_ns (void)
+int gethostbyname_r(const char *name, 
+		struct hostent *ret, char *buf, size_t buflen,
+		struct hostent **result, int *h_errnop)
 {
-	int i;
-	for(i=0;i<_res.nscount;i++) {
-		char buf[INET_ADDRSTRLEN];
-
-		inet_ntop(AF_INET, &_res.nsaddr_list[i].sin_addr, buf, sizeof(buf));
-		fprintf(stderr, "dnssrvr: %s\n", buf);
-	}
-}
-#endif
-
-static void
-override_ns (void)
-{
-	int i;
-	int valid_ns = 0;
-
-	for (i = 0; i < MAXNS; i++) {
-		char envvar[] = "NAMESERVERx";
-		const char *ns;
-
-		/* Rename to NAMESERVERx where x is the name server number,
-		 * and the first one is NAMESERVER1 */
-		envvar[10] = '1' + i;
-
-		ns = getenv (envvar);
-		if (ns == NULL)
-			break;
-		if (inet_pton (AF_INET, ns, &_res.nsaddr_list[i].sin_addr) < 1) {
-			fprintf (stderr, "Ignoring invalid nameserver '%s'\n", ns);
-			continue;
+	int (*f)(const char *name, struct hostent *, char *, size_t, struct hostent **, int *) = dlsym (RTLD_NEXT, "gethostbyname_r");
+	int r = f(name, ret, buf, buflen, result, h_errnop);
+	if (r == 0 && ret->h_length == 0) {
+		char new_name[1024];
+		const char *domain;
+		domain = getenv("DOMAIN");
+		if (!domain) {
+			fprintf(stderr, "DOMAIN environment variable not found!\n");
+			exit(-1);
 		}
-
-		valid_ns++;
+		sprintf(new_name, "%s.%s", name, domain);
+		r =  f(new_name, ret, buf, buflen, result, h_errnop);
 	}
-
-	/* Set the number of valid nameservers */
-	if (valid_ns > 0)
-		_res.nscount = valid_ns;
-}
-
-struct hostent *gethostbyname(const char *name)
-{
-	if (res_init () < 0)
-		return NULL;
-	struct hostent * (*f)() = dlsym (RTLD_NEXT, "gethostbyname");
-	struct hostent *ret =  f(name);
-
-	return ret;
-}
-
-int getaddrinfo(const char *node, const char *service,
-		const struct addrinfo *hints,
-		struct addrinfo **res)
-{
-	if (res_init () < 0)
-		return EAI_SYSTEM;
-	int (*f)() = dlsym (RTLD_NEXT, "getaddrinfo");
-	return f(node, service, hints, res);
-}
-
-int __res_init(void)
-{
-	int (*f)() = dlsym (RTLD_NEXT, "__res_init");
-	assert (f);
-	int ret = f();
-
-	override_ns ();
-
-	return ret;
+	return r;
 }
